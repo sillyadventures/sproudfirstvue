87a24d77753d46906bfcbf7336294102aaab8717
Ru Chern Chong <ruchern@sproud.biz>
2018-01-15 17:41:04 +0800

    Format code
    Signed-off-by: Ru Chern Chong <ruchern@sproud.biz>

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Home Page</h1>
-        <input type="text" id="input" v-model="msg">
+        <input type="text" id="input" v-model="msg"/>
         <h2>{{ msg }}</h2>
         <h3>This is pretty cool huh</h3>
     </div>
@@ -9,11 +9,11 @@
 
 <script>
 export default {
-  name: 'HomePage',
-  data () {
+  name: "HomePage",
+  data() {
     return {
-      msg: 'Type Something Here'
-    }
+      msg: "Type Something Here"
+    };
   }
-}
+};
 </script>
