cf805cb46dd952b7f4e4e26092fb9a92033d4183
Ru Chern Chong <ruchern@sproud.biz>
2018-01-15 18:45:03 +0800

    Change root path to a different component
    Signed-off-by: Ru Chern Chong <ruchern@sproud.biz>

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import HomePage from '@/components/HomePage'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/home',
       name: 'HomePage',
       component: HomePage
     }
