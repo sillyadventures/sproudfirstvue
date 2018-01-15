c70c3f71f47eca1a3df82db42d09ce3f836a2773
Shui Loong <shuiloong@gmail.com>
2018-01-15 15:55:35 +0800

    added a new route /home

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <h2>{{ msg }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      msg: 'Welcome !!'
+    }
+  }
+}
+</script>
