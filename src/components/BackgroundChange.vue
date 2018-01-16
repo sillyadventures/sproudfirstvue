bbab8b73ec780e53feeba51abe078acf54d01343
Ru Chern Chong <ruchern@sproud.biz>
2018-01-16 12:56:49 +0800

    Clean up
    Signed-off-by: Ru Chern Chong <ruchern@sproud.biz>

--- src/components/BackgroundChange.vue
+++ src/components/BackgroundChange.vue
@@ -5,7 +5,7 @@
         <br>
         <button @click="change">Add to Counter</button>
         <p>The button above has been clicked {{ counter }} times.</p>
-        <div id="boxmove">
+        <div id="box-move">
             <div id="box"></div>
         </div>
     </div>
@@ -18,43 +18,36 @@
       return {
         counter: 0,
         move: 0,
-        moveup: true,
-        msg: 'You loaded this page on ' + new Date().toLocaleString(),
+        moveUp: true,
+        msg: `You loaded this page on ${new Date().toLocaleString()}`
       }
     },
     methods: {
       changeCounter () {
-        this.counter += 1
+        this.counter++
       },
       changeColor () {
         const x = Math.floor(Math.random() * 256)
         const y = Math.floor(Math.random() * 256)
         const z = Math.floor(Math.random() * 256)
-        const bgColor = `rgb(${x},${y},${z})`
-        const c = document.getElementById('count')
-        c.style.background = bgColor
+
+        document.getElementById('count').style.background = `rgb(${x},${y},${z})`
       },
       changeRotate () {
-        const b = document.getElementById('box')
-        const degree = `rotate(${this.counter * 15}deg)`
-        b.style.transform = degree
+        document.getElementById('box').style.transform = `rotate(${this.counter * 15}deg)`
       },
       changeTranslate () {
-        const m = document.getElementById('boxmove')
-        if (this.moveup) {
+        if (this.moveUp) {
           this.move++
-          if (this.move >= 18)
-            this.moveup = false
-        }
-        else {
+
+          if (this.move >= 18) this.moveUp = false
+        } else {
           this.move--
-          if (this.move <= 0)
-            this.moveup = true
+
+          if (this.move <= 0) this.moveUp = true
         }
 
-        const translate = `translate(${this.move * 5}%)`
-        console.log(this.moveup)
-        m.style.transform = translate
+        document.getElementById('box-move').style.transform = `translate(${this.move * 5}%)`
       },
       change () {
         this.changeCounter()
