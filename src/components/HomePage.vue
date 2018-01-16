2525b76fa8d783abe024a8f329dcd2f45531d5a6
Ru Chern Chong <ruchern@sproud.biz>
2018-01-16 13:23:20 +0800

    Add click to clear input and router link
    Signed-off-by: Ru Chern Chong <ruchern@sproud.biz>

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="home">
         <h1>Home Page</h1>
-        <input type="text" id="input" v-model="msg"/>
+        <input type="text" id="input" v-model="msg" placeholder="Type something here" @click="clearInput"/>
         <h2>{{ msg }}</h2>
+
+        <router-link to="/change">Go next</router-link>
     </div>
 </template>
 
@@ -11,7 +13,12 @@
     name: 'HomePage',
     data () {
       return {
-        msg: 'Type Something Here'
+        msg: null
+      }
+    },
+    methods: {
+      clearInput () {
+        this.msg = null
       }
     }
   }
